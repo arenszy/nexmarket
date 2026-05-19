969da9058ed2fd8abac8d98fad61e73f662cb476
arya <kashimaryuuto17@gmail.com>
2026-05-19 14:52:14 +0700

    add-netlify-config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
@@ -8,9 +7,6 @@ const nextConfig = {
       { protocol: 'https', hostname: 'via.placeholder.com' },
       { protocol: 'https', hostname: 'picsum.photos' },
     ],
-  },
-  experimental: {
-    serverActions: { allowedOrigins: ['localhost:3000'] },
   },
 }
 
